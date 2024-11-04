5f959b345864bebd7c6c8b23087a464daedb1f60
atarrach <austin.tarrach@wsu.edu>
2024-11-04 10:13:34 -0800

    Added basic functionality for the Drink class and Drink List class. Will be adding more later in relation to the drink algorithm

---
+++ Drink-Class-Library/Drink.cs
@@ -0,0 +1,43 @@
+﻿namespace Drink_Class_Library
+{
+    public class Drink
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public double price;
+        private double initial_price;
+        private double percentage_threshold;
+        private double initial_threshold;
+
+        private int price_level;
+
+        public Drink(int ID, string drink_name, double price)
+        {
+            this.ID = ID;
+            this.name = drink_name;
+            this.price = price;
+            this.initial_price = price;
+            this.percentage_threshold = -1.0;
+            this.initial_threshold = -1.0;
+            this.price_level = 0;
+
+        }
+
+        public double increase_price()
+        {
+            this.price_level++;
+            this.price += .25;
+
+            return this.price;
+
+        }
+
+        public double decrease_price()
+        {
+            this.price_level--;
+            this.price -= .25;
+
+            return this.price;
+        }
+    }
+}
---
+++ Drink-Class-Library/DrinkList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drink_Class_Library
+{
+    internal class DrinkList
+    {
+
+        private List<Drink> drinks;
+
+        public DrinkList()
+        {
+            drinks = new List<Drink>();
+        }
+
+        public void AddDrink(Drink drink)
+        {
+            drinks.Add(drink);
+        }
+
+        private Drink FindDrinkByName(string name)
+        {
+            return drinks.Find(d => d.name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void DisplayDrinks()
+        {
+            foreach (var drink in drinks)
+            {
+                Console.WriteLine(drink);
+            }
+        }
+    }
+}
