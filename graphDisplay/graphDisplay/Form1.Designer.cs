98ce0446cb2a5567a66865e3f1f6f19826a8ddf5
Khoa Ta <khoatatd@gmail.com>
2024-11-09 13:33:28 -0800

    Added scrolling animation to the highlight bar. Make the graph looks nicer

--- graphDisplay/graphDisplay/Form1.Designer.cs
+++ graphDisplay/graphDisplay/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp1
+﻿using System.Runtime.CompilerServices;
+
+namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -28,105 +30,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.priceGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.drinkHightlight = new System.Windows.Forms.Panel();
-            this.highlight5 = new System.Windows.Forms.Label();
-            this.highlight4 = new System.Windows.Forms.Label();
-            this.hightlight3 = new System.Windows.Forms.Label();
-            this.hightlight2 = new System.Windows.Forms.Label();
+            this.scrollAnimationTimer = new System.Windows.Forms.Timer(this.components);
             this.highlight1 = new System.Windows.Forms.Label();
+            this.highlight2 = new System.Windows.Forms.Label();
+            this.highlight3 = new System.Windows.Forms.Label();
+            this.highlight4 = new System.Windows.Forms.Label();
+            this.highlight5 = new System.Windows.Forms.Label();
+            this.drinkHighlight = new System.Windows.Forms.Panel();
+            this.scrollTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.priceGraph)).BeginInit();
-            this.drinkHightlight.SuspendLayout();
+            this.drinkHighlight.SuspendLayout();
             this.SuspendLayout();
             // 
             // priceGraph
             // 
-            chartArea4.Name = "ChartArea1";
-            this.priceGraph.ChartAreas.Add(chartArea4);
+            chartArea2.AxisX.LabelStyle.Enabled = false;
+            chartArea2.AxisX.MajorGrid.Enabled = false;
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.AxisY2.Crossing = 1.7976931348623157E+308D;
+            chartArea2.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisY2.MajorGrid.Enabled = false;
+            chartArea2.Name = "ChartArea1";
+            this.priceGraph.ChartAreas.Add(chartArea2);
             this.priceGraph.Cursor = System.Windows.Forms.Cursors.Default;
-            legend4.Name = "Legend1";
-            this.priceGraph.Legends.Add(legend4);
-            this.priceGraph.Location = new System.Drawing.Point(0, 1);
+            this.priceGraph.Dock = System.Windows.Forms.DockStyle.Top;
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.priceGraph.Legends.Add(legend2);
+            this.priceGraph.Location = new System.Drawing.Point(0, 0);
             this.priceGraph.Name = "priceGraph";
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.priceGraph.Series.Add(series4);
-            this.priceGraph.Size = new System.Drawing.Size(788, 419);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.priceGraph.Series.Add(series2);
+            this.priceGraph.Size = new System.Drawing.Size(790, 443);
             this.priceGraph.TabIndex = 0;
             this.priceGraph.Text = "priceGraph";
             // 
-            // drinkHightlight
+            // scrollAnimationTimer
             // 
-            this.drinkHightlight.Controls.Add(this.highlight5);
-            this.drinkHightlight.Controls.Add(this.highlight4);
-            this.drinkHightlight.Controls.Add(this.hightlight3);
-            this.drinkHightlight.Controls.Add(this.hightlight2);
-            this.drinkHightlight.Controls.Add(this.highlight1);
-            this.drinkHightlight.Location = new System.Drawing.Point(0, 414);
-            this.drinkHightlight.Name = "drinkHightlight";
-            this.drinkHightlight.Size = new System.Drawing.Size(788, 37);
-            this.drinkHightlight.TabIndex = 1;
-            this.drinkHightlight.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // highlight5
-            // 
-            this.highlight5.Location = new System.Drawing.Point(638, 2);
-            this.highlight5.Name = "highlight5";
-            this.highlight5.Size = new System.Drawing.Size(150, 35);
-            this.highlight5.TabIndex = 4;
-            this.highlight5.Text = "highlight5";
-            this.highlight5.Click += new System.EventHandler(this.highlight5_Click);
-            // 
-            // highlight4
-            // 
-            this.highlight4.Location = new System.Drawing.Point(466, 2);
-            this.highlight4.Name = "highlight4";
-            this.highlight4.Size = new System.Drawing.Size(166, 35);
-            this.highlight4.TabIndex = 3;
-            this.highlight4.Text = "highlight4";
-            this.highlight4.Click += new System.EventHandler(this.highlight4_Click);
-            // 
-            // hightlight3
-            // 
-            this.hightlight3.Location = new System.Drawing.Point(310, 2);
-            this.hightlight3.Name = "hightlight3";
-            this.hightlight3.Size = new System.Drawing.Size(150, 35);
-            this.hightlight3.TabIndex = 2;
-            this.hightlight3.Text = "highlight3";
-            // 
-            // hightlight2
-            // 
-            this.hightlight2.Location = new System.Drawing.Point(154, 2);
-            this.hightlight2.Name = "hightlight2";
-            this.hightlight2.Size = new System.Drawing.Size(150, 35);
-            this.hightlight2.TabIndex = 1;
-            this.hightlight2.Text = "highlight2";
+            this.scrollAnimationTimer.Tick += new System.EventHandler(this.scrollAnimationTimer_Tick);
             // 
             // highlight1
             // 
-            this.highlight1.Location = new System.Drawing.Point(-2, 2);
+            this.highlight1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highlight1.Location = new System.Drawing.Point(-2, 0);
             this.highlight1.Name = "highlight1";
-            this.highlight1.Size = new System.Drawing.Size(150, 35);
+            this.highlight1.Size = new System.Drawing.Size(150, 30);
             this.highlight1.TabIndex = 0;
             this.highlight1.Text = "highlight1";
-            this.highlight1.Click += new System.EventHandler(this.label1_Click);
+            this.highlight1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // highlight2
+            // 
+            this.highlight2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highlight2.Location = new System.Drawing.Point(154, 0);
+            this.highlight2.Name = "highlight2";
+            this.highlight2.Size = new System.Drawing.Size(150, 30);
+            this.highlight2.TabIndex = 1;
+            this.highlight2.Text = "highlight2";
+            this.highlight2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // highlight3
+            // 
+            this.highlight3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highlight3.Location = new System.Drawing.Point(310, 0);
+            this.highlight3.Name = "highlight3";
+            this.highlight3.Size = new System.Drawing.Size(150, 30);
+            this.highlight3.TabIndex = 2;
+            this.highlight3.Text = "highlight3";
+            this.highlight3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // highlight4
+            // 
+            this.highlight4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highlight4.Location = new System.Drawing.Point(468, 0);
+            this.highlight4.Name = "highlight4";
+            this.highlight4.Size = new System.Drawing.Size(166, 30);
+            this.highlight4.TabIndex = 3;
+            this.highlight4.Text = "highlight4";
+            this.highlight4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // highlight5
+            // 
+            this.highlight5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highlight5.Location = new System.Drawing.Point(640, 0);
+            this.highlight5.Name = "highlight5";
+            this.highlight5.Size = new System.Drawing.Size(150, 30);
+            this.highlight5.TabIndex = 4;
+            this.highlight5.Text = "highlight5";
+            this.highlight5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // drinkHighlight
+            // 
+            this.drinkHighlight.Controls.Add(this.highlight5);
+            this.drinkHighlight.Controls.Add(this.highlight4);
+            this.drinkHighlight.Controls.Add(this.highlight3);
+            this.drinkHighlight.Controls.Add(this.highlight2);
+            this.drinkHighlight.Controls.Add(this.highlight1);
+            this.drinkHighlight.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.drinkHighlight.Location = new System.Drawing.Point(0, 425);
+            this.drinkHighlight.Name = "drinkHighlight";
+            this.drinkHighlight.Size = new System.Drawing.Size(790, 30);
+            this.drinkHighlight.TabIndex = 1;
+            this.drinkHighlight.Resize += new System.EventHandler(this.drinkHighlights_Resize);
+            // 
+            // scrollTimer
+            // 
+            this.scrollTimer.Enabled = true;
+            this.scrollTimer.Interval = 5000;
+            this.scrollTimer.Tick += new System.EventHandler(this.ChangeTextWithVerticalScroll);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(790, 455);
-            this.Controls.Add(this.drinkHightlight);
+            this.Controls.Add(this.drinkHighlight);
             this.Controls.Add(this.priceGraph);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.priceGraph)).EndInit();
-            this.drinkHightlight.ResumeLayout(false);
+            this.drinkHighlight.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -134,12 +165,14 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart priceGraph;
-        private System.Windows.Forms.Panel drinkHightlight;
+        private System.Windows.Forms.Timer scrollAnimationTimer;
         private System.Windows.Forms.Label highlight1;
+        private System.Windows.Forms.Label highlight2;
+        private System.Windows.Forms.Label highlight3;
         private System.Windows.Forms.Label highlight4;
-        private System.Windows.Forms.Label hightlight3;
-        private System.Windows.Forms.Label hightlight2;
         private System.Windows.Forms.Label highlight5;
+        private System.Windows.Forms.Panel drinkHighlight;
+        private System.Windows.Forms.Timer scrollTimer;
     }
 }
 
--- graphDisplay/graphDisplay/Form1.cs
+++ graphDisplay/graphDisplay/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,20 +15,38 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private int textPosition = 0;
+        private string[] highlightText = { "Highlight1", "Highlight2", "Highlight3", "Highlight4", "Highlight5" };
+
+        private bool[] scrollingUp;
+        private int[] initialY;
+        private int[] targetY;
+
+        private System.Windows.Forms.Label[] highlightLabels;
+
         public Form1()
         {
             InitializeComponent();
             SetupChart();
+
+            drinkHighlight.Resize += drinkHighlights_Resize;
+            highlightLabels = new System.Windows.Forms.Label[] {highlight1, highlight2, highlight3, highlight4, highlight5};
+            initialY = new int[] { 0, 0, 0, 0, 0 };
+            targetY = new int[] { 0, 0, 0, 0, 0 };
+            scrollingUp = new bool[] {false, false, false, false, false };
+
+            for (int i=0; i<5; i++)
+            {
+                highlightLabels[i].Text = highlightText[i];
+                initialY[i] = highlightLabels[i].Top;
+            }
+
+            scrollAnimationTimer.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        }
-
-        private void priceGraph_Click(object sender, EventArgs e)
-        {
-            
         }
 
         private void SetupChart()
@@ -53,24 +73,76 @@ namespace WindowsFormsApp1
             series.Points.AddXY(5, 30);
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void scrollAnimationTimer_Tick(object sender, EventArgs e)
         {
+            for (int i = 0; i<5; i++)
+            {
+                if (scrollingUp[i])
+                {
+                    // Move label upwards
+                    highlightLabels[i].Top -= 5; // Adjust this speed if needed
 
+                    // Check if it’s out of view
+                    if (highlightLabels[i].Top <= targetY[i])
+                    {
+                        highlightLabels[i].Top = initialY[i] + highlightLabels[i].Height;  // Reset position below
+                        highlightLabels[i].Text = highlightText[i];                         // Update text
+                        scrollingUp[i] = false;                                  // Now scroll it into view
+                    }
+                }
+                else
+                {
+                    // Move label upwards to bring it into view
+                    highlightLabels[i].Top -= 5;  // Adjust speed as needed
+
+                    // Stop animation when it reaches original position
+                    if (highlightLabels[i].Top <= initialY[i])
+                    {
+                        highlightLabels[i].Top = initialY[i];   // Ensure it lands exactly
+                        scrollAnimationTimer.Stop();             // Stop the timer
+                    }
+                }
+            }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private string[] GetHighlightText()
         {
-
+            string[] text = new string[5] { "Rum 10.00 \u25B2", "Budlight 5.00 \u25BC", "Corona 4.50 \u25B2", "ESP Martini 17.30 \u25BC", "Heineken 4.70 \u25B2" };
+            return text;
         }
 
-        private void highlight5_Click(object sender, EventArgs e)
+        public void ChangeTextWithVerticalScroll(object sender, EventArgs e)
         {
-
+            string[] text = GetHighlightText();
+            for (int i = 0; i < 5; i++)
+            {
+                highlightText[i] = text[i];
+                targetY[i] = initialY[i] - highlightLabels[i].Height;  // Target position above
+                scrollingUp[i] = true;                         // Start scrolling up
+                scrollAnimationTimer.Start();
+            }
         }
 
-        private void highlight4_Click(object sender, EventArgs e)
+        private void drinkHighlights_Resize(object sender, EventArgs e)
         {
-
+            DistributeLabelsHorizontally();
         }
+
+        private void DistributeLabelsHorizontally()
+        {
+            int panelWidth = drinkHighlight.ClientSize.Width;
+            int labelCount = highlightLabels.Length;
+
+            // Calculate the horizontal space between each label
+            int spacing = panelWidth / (labelCount + 1);
+
+            for (int i = 0; i < labelCount; i++)
+            {
+                // Position each label with calculated spacing
+                highlightLabels[i].Left = spacing * (i + 1) - (highlightLabels[i].Width / 2); // Center each label horizontally
+                highlightLabels[i].Top = (drinkHighlight.ClientSize.Height - highlightLabels[i].Height) / 2; // Center vertically in panel
+            }
+        }
+
     }
 }
